764c62dccdbe47eabc9679741a31cd99a05006e9
phserviceiptv-create <phserviceiptv@gmail.com>
2026-09-02 11:51:59 -0300

    chore: configure Next.js

---
+++ next.config.ts
@@ -0,0 +1,3 @@
+import type { NextConfig } from 'next';
+const nextConfig: NextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: 'zjeclsozvjymuzwyhvqj.supabase.co' }, { protocol: 'https', hostname: 'hey-roll-lets-go.vercel.app' }] } };
+export default nextConfig;
